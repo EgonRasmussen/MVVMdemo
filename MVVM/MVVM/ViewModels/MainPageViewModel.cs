c9af9a074e4583956715077ddfdfb276dbb6f3af
ECR\Egon Rasmussen <ecr@live.dk>
2020-10-19 14:55:01 +0200

    4.Messaging

--- MVVM/MVVM/ViewModels/MainPageViewModel.cs
+++ MVVM/MVVM/ViewModels/MainPageViewModel.cs
@@ -1,16 +1,20 @@
-﻿// Nu er der lavet en ViewModelBase klasse, der lader Change Property Notification nedarve til alle ViewModels
+﻿// Her er tilføjet Commanding Properties, som der bindes til
+// i View'et. Bemærk at DeleteCommand er lavet med en anonymous 
+// metode, hvor imod alle de andre er lavet med en normal metode.
+// Der er dog et problem med ShowAgeCommand, som skal åbne en 
+// DisplayAlert i View'et - men det kan ikke umiddelbart virke.
+// Så er det godt at vi har Messages (se næste branch).
 
 using MVVM.Models;
+using System;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 
 namespace MVVM.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
         public ObservableCollection<Person> Persons { get; }
-
 
         #region CONSTRUCTOR
         public MainPageViewModel()
@@ -21,6 +25,37 @@ namespace MVVM.ViewModels
                     new Person { Name = "Christian", Age = 32 },
                     new Person { Name = "Helle", Age = 54 }
                 };
+
+            MakeOlderCommand = new Command(
+                execute: () =>
+                {
+                    Age++;
+                    _personSelectedItem.Age = Age;
+                    RefreshCanExecutes();
+                },
+                canExecute: () =>
+                {
+                    return _personSelectedItem != null;
+                });
+
+            ClearEntriesCommand = new Command(
+               execute: () =>
+               {
+                   Name = string.Empty;
+                   Age = 0;
+               });
+
+            AddCommand = new Command(
+               execute: () => Persons.Add(new Person { Name = Name, Age = Age }),
+               canExecute: () =>
+               {
+                   return Name?.Length > 0 && Age > 0;
+               });
+
+            ShowAgeCommand = new Command(
+                execute: () => App.Current.MainPage.DisplayAlert("AgeButtonClicked", "Alder: " + PersonSelectedItem.Age, "OK"),
+                canExecute: () => _personSelectedItem != null
+                );
         }
         #endregion
 
@@ -35,6 +70,7 @@ namespace MVVM.ViewModels
                 {
                     Name = value.Name;
                     Age = value.Age;
+                    RefreshCanExecutes();
                 }
             }
         }
@@ -43,14 +79,53 @@ namespace MVVM.ViewModels
         public string Name
         {
             get { return _name; }
-            set { SetProperty(ref _name, value); }
+            set { SetProperty(ref _name, value); RefreshCanExecutes(); }
         }
 
         int _age;
         public int Age
         {
             get { return _age; }
-            set { SetProperty(ref _age, value); }
+            set { SetProperty(ref _age, value); RefreshCanExecutes(); }
+        }
+        #endregion
+
+        #region COMMANDING
+        // Properties for implementing commands in constructor.
+        public Command MakeOlderCommand { get; private set; }
+
+        public Command AddCommand { get; private set; }
+
+        public Command ClearEntriesCommand { get; private set; }
+
+        public Command ShowAgeCommand { get; private set; }
+
+
+        // Property for local implementation (an alternative syntax).
+        public Command _onDeleteCommand;
+        public Command DeleteCommand
+        {
+            get
+            {
+                return _onDeleteCommand ?? (_onDeleteCommand = new Command(
+                    execute: () =>
+                    {
+                        Persons.Remove(_personSelectedItem ?? null);
+                    },
+                    canExecute: () =>
+                    {
+                        return _personSelectedItem != null && Persons.Count > 1;
+                    }
+                    ));
+            }
+        }
+
+        void RefreshCanExecutes()
+        {
+            DeleteCommand.ChangeCanExecute();
+            MakeOlderCommand.ChangeCanExecute();
+            AddCommand.ChangeCanExecute();
+            ShowAgeCommand.ChangeCanExecute();
         }
         #endregion
     }
